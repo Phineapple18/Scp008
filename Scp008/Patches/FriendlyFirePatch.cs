eab36c745be0dcdf4dfef0ad4b0ff51ffb3e3090
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2024-06-30 20:50:14 +0200

    Add files via upload

---
+++ Scp008/Patches/FriendlyFirePatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+namespace Scp008.Patches
+{
+    [HarmonyPatch("FriendlyFireHandler", "IsFriendlyFire")]
+    internal class FriendlyFirePatch
+    {
+        internal static void Postfix(ReferenceHub damagedPlayer, ref bool __result)
+        {
+            if (__result && damagedPlayer.IsScp008())
+            {
+                __result = false;
+            }
+        }
+    }
+}
